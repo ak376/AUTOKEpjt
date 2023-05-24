325e5acfd1aa8a7656f34a4f271aa8a370b81409
ak376 <real980312@gmail.com>
2023-05-24 09:05:32 +0900

    get, post

--- kfcweb/server.js
+++ kfcweb/server.js
@@ -1,46 +1,95 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+let app = express();
+const port = 3000;
 const cors = require("cors");
 const pool = require("./db");
 
-const http = require('http')
-const server = http.createServer(app)
-const { Server } = require('socket.io');
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
 const io = new Server(server, {
-  cors:{
-    origin: true
+  cors: {
+    origin: true,
   },
   pingInterval: 100, //100 ms
-  pingTimeout: 1
+  pingTimeout: 1,
 });
 
 app.use(
-    cors({
-        origin: true
-    })
-);
-
-app.get('/email', async (req, res) => {
-  var data= req.param('add');
-  const ok = await pool.query("SHOW TABLES LIKE '"+data+"'");
-  if(ok[0]==''){
-    const mama = await pool.query("CREATE TABLE `userDB`.`" +data+"` (`email` VARCHAR(45) NOT NULL, `nick` VARCHAR(45) NOT NULL, `ip` VARCHAR(45) NOT NULL, `id` INT NOT NULL, PRIMARY KEY (`id`)));
-  }
-  //const ood = await pool.query("select * from "+data );
-  console.log(ok);
-  res.send(ok[0]); 
-})
-
-io.on("connection", async (socket) => {
-    const ret = await pool.query("select * from sensing order by time desc limit 10");
-    socket.emit("fromser", ret[0]);
-  
-    socket.on("bbq", (arg) => {
-      console.log(arg);
-    })
+  cors({
+    origin: true,
+  }),
+  express.urlencoded({
+    extended: true,
   })
+);
+let ok;
+app.get("/", async (req, res) => {
+  const table = await pool.query("SHOW TABLES FROM userDB");
+
+  res.send(table[0]);
+});
+app.get("/email", async (req, res) => {
+  const data = req.param("add");
+
+  try {
+    ok = await pool.query("SELECT * FROM `userDB`." + data);
+    const userdata = { result: ok[0] };
+    //console.log(res);
+    res.send(userdata);
+  } catch (error) {
+    try {
+      const ok2 = await pool.query(
+        "CREATE TABLE `userDB`." +
+          data +
+          " ( `nick` VARCHAR(45) NOT NULL, `ip` VARCHAR(45) NOT NULL, `id` INT NOT NULL AUTO_INCREMENT, PRIMARY KEY (`id`))"
+      );
+
+      res.send("create user");
+    } catch (error) {
+      res.send("errorrrrr");
+    }
+  }
+});
+
+app.post("/add", async (req, res) => {
+  const { email, nick, ip } = req.body;
+  console.log(req.userid);
+  try {
+    ok = await pool.query(
+      "INSERT INTO " + email + "(nick, ip) values (?, ?);",
+      [nick, ip]
+    );
+    const userdata = { result: ok[0] };
+    //console.log(res);
+    res.send(userdata);
+  } catch (error) {
+    res.send(error);
+  }
+});
+
+app.post("/delete", async (req, res) => {
+  const { email, index } = req.body;
+  console.log(email, index);
+  try {
+    ok = await pool.query(
+      "delete from " +
+        email +
+        " where id = (select id from(select row_number() over(order by id ) as num, nick, ip, id from " +
+        email +
+        ") as a where a.num=" +
+        index +
+        ")"
+    );
+    const userdata = { result: ok[0] };
+    //console.log(res);
+    res.send(userdata);
+  } catch (error) {
+    res.send(error);
+  }
+});
 
 server.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
+
